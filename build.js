433844bf50cf630e30ba1174a03ed8001a0f130c
Steven <steven@ceriously.com>
2019-04-15 18:29:52 -0400

    Add typescript

--- build.js
+++ build.js
@@ -5,12 +5,12 @@ const writeFile = promisify(fs.writeFile);
 const ncc = require('@zeit/ncc');
 
 async function main() {
-	const input = './index.js';
+	const input = './index.ts';
 	const outDir = './dist';
 	const opts = { sourceMap: true, sourceMapRegister: true };
 	const { code, map, assets } = await ncc(input, opts);
-	await writeFile(join(outDir, input), code);
-	await writeFile(join(outDir, `${input}.map`), map);
+	await writeFile(join(outDir, input.replace('.ts', '.js')), code);
+	await writeFile(join(outDir, `${input.replace('.ts', '.js')}.map`), map);
 	for (var [assetName, assetCode] of Object.entries(assets)) {
 		await writeFile(join(outDir, assetName), assetCode.source.toString('utf8'));
 	}
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-function handler(req, res) {
+export default function handler(req: any, res: any) {
   try {
     if (req) {
       throw new Error(`Should throw`);
